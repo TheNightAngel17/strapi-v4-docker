a0739198b08bb694060cb0190a5a0d8a3bd6b7df
Mitchell Lemons <goalkeeper171717@gmail.com>
2022-09-30 00:10:50 -0500

    initial commit

---
+++ config/database.js
@@ -0,0 +1,66 @@
+const postgres = {
+  client: 'postgres',
+  connection: {
+    database: process.env.DATABASE_NAME ?? 'strapi',
+    user: process.env.DATABASE_USERNAME ?? 'strapi',
+    password: process.env.DATABASE_PASSWORD ?? 'strapi',
+    port: process.env.DATABASE_PORT ?? 5432,
+    host: process.env.DATABASE_HOST ?? 'localhost',
+  },
+};
+
+const mysql = {
+  client: 'mysql',
+  connection: {
+    database: process.env.DATABASE_NAME ?? 'strapi',
+    user: process.env.DATABASE_USERNAME ?? 'strapi',
+    password: process.env.DATABASE_PASSWORD ?? 'strapi',
+    port: process.env.DATABASE_PORT ?? 3306,
+    host: process.env.DATABASE_HOST ?? 'localhost',
+  },
+};
+
+const sqlite = {
+  client: 'sqlite',
+  connection: {
+    filename: '.tmp/data.db',
+  },
+  useNullAsDefault: true,
+};
+
+const db = {
+  mysql,
+  sqlite,
+  postgres,
+};
+
+
+module.exports = {
+  connection: process.env.DB ? db[process.env.DB] || db.sqlite : db.sqlite,
+};
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: env("DATABASE_CLIENT", "postgres"),
+
+//     connection: {
+//       host: env("DATABASE_HOST", "127.0.0.1"),
+//       port: env.int("DATABASE_PORT", 5432),
+//       database: env("DATABASE_NAME", "strapi"),
+//       user: env("DATABASE_USERNAME", "strapi"),
+//       password: env("DATABASE_PASSWORD", "strapi"),
+//     },
+//     debug: false,
+//   },
+// });
+
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'sqlite',
+//     connection: {
+//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+//     },
+//     useNullAsDefault: true,
+//   },
+// });
