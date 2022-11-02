e924da2c5d044b7c14f0ca9fcb80aaa816889048
Mitchell Lemons <goalkeeper171717@gmail.com>
2022-11-02 11:19:03 -0500

    udating pipelines for different dockerfiles

--- config/database.js
+++ config/database.js
@@ -39,28 +39,3 @@ module.exports = {
   connection: process.env.DATABASE_CLIENT ? db[process.env.DATABASE_CLIENT] || db.sqlite : db.sqlite,
 };
 
-// module.exports = ({ env }) => ({
-//   connection: {
-//     client: env("DATABASE_CLIENT", "postgres"),
-
-//     connection: {
-//       host: env("DATABASE_HOST", "127.0.0.1"),
-//       port: env.int("DATABASE_PORT", 5432),
-//       database: env("DATABASE_NAME", "strapi"),
-//       user: env("DATABASE_USERNAME", "strapi"),
-//       password: env("DATABASE_PASSWORD", "strapi"),
-//     },
-//     debug: false,
-//   },
-// });
-
-
-// module.exports = ({ env }) => ({
-//   connection: {
-//     client: 'sqlite',
-//     connection: {
-//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-//     },
-//     useNullAsDefault: true,
-//   },
-// });
